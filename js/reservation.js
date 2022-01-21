2ac05d3bf2f60b7930577367a79c8492801de07d
ljubicamiljkovic56 <ljubica.m19@gmail.com>
2022-01-21 21:37:07 +0100

    Reservation page finished

---
+++ js/reservation.js
@@ -0,0 +1,54 @@
+$(document).ready(function (e) {
+    $('#S01,#S02,#S03,#S04,#S05,#S06,#S07,#S08,#S09,#S10').click(function (e) {
+        if($(this).prop("checked") == true) {
+            alert("The seat is successfully reserved!")
+        }else if($(this).prop("checked") == false){
+            alert("Do you want to cancel the reservation?")
+        }
+    });
+});
+$(document).ready(function (e) {
+    $('#S11,#S12,#S13,#S14,#S15,#S16,#S17,#S18,#S19,#S20').click(function (e) {
+        if($(this).prop("checked") == true) {
+            alert("The seat is successfully reserved!")
+        }else if($(this).prop("checked") == false){
+            alert("Do you want to cancel the reservation?")
+        }
+    });
+});
+$(document).ready(function (e) {
+    $('#S21,#S22,#S23,#S24,#S25,#S26,#S27,#S28,#S29,#S30').click(function (e) {
+        if($(this).prop("checked") == true) {
+            alert("The seat is successfully reserved!")
+        }else if($(this).prop("checked") == false){
+            alert("Do you want to cancel the reservation?")
+        }
+    });
+});
+$(document).ready(function (e) {
+    $('#S31,#S32,#S33,#S34,#S35,#S36,#S37,#S38,#S39,#S40').click(function (e) {
+        if($(this).prop("checked") == true) {
+            alert("The seat is successfully reserved!")
+        }else if($(this).prop("checked") == false){
+            alert("Do you want to cancel the reservation?")
+        }
+    });
+});
+$(document).ready(function (e) {
+    $('#S41,#S42,#S43,#S44,#S45,#S46,#S47,#S48,#S49,#S50').click(function (e) {
+        if($(this).prop("checked") == true) {
+            alert("The seat is successfully reserved!")
+        }else if($(this).prop("checked") == false){
+            alert("Do you want to cancel the reservation?")
+        }
+    });
+});
+$(document).ready(function (e) {
+    $('#S51,#S52,#S53,#S54,#S55,#S56,#S57,#S58,#S59,#S60').click(function (e) {
+        if($(this).prop("checked") == true) {
+            alert("The seat is successfully reserved!")
+        }else if($(this).prop("checked") == false){
+            alert("Do you want to cancel the reservation?")
+        }
+    });
+});
